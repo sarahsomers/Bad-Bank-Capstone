3920d108816c0cc4a9e2b9a9353440dba6d0ad4e
Sarah Somers <sarahsomers82@yahoo.com>
2023-12-21 09:50:02 -0800

    Mongo

--- dal.js
+++ dal.js
@@ -1,14 +1,38 @@
-const MongoClient = require('mongodb').MongoClient;
-const url         = 'mongodb://localhost:3000';
-let db            = null;
+// const MongoClient = require('mongodb').MongoClient;
+// const url         = 'mongodb://localhost:3000';
+// let db            = null;
  
-// connect to mongo
-MongoClient.connect(url, {useUnifiedTopology: true}, function(err, client) {
-    console.log("Connected successfully to db server");
+// // connect to mongo
+// MongoClient.connect(url, {useUnifiedTopology: true}, function(err, client) {
+//     console.log("Connected successfully to db server");
 
-    // connect to myproject database
-    db = client.db('myproject');
+//     // connect to myproject database
+//     db = client.db('myproject');
+// });
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://<username>:<password>@badbank.svsyh6u.mongodb.net/?retryWrites=true&w=majority";
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
 });
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
 
 // create user account
 function create(name, email, password){
--- mongo_test.js
+++ mongo_test.js
@@ -1,33 +1,56 @@
-const MongoClient = require('mongodb').MongoClient;
-const url = 'mongodb://localhost:3000';
- 
-// connect to mongo
-MongoClient.connect(url, {useUnifiedTopology: true}, function(err, client) {
-  console.log("Connected successfully to server");
-
-    // database Name
-    const dbName = 'myproject';
-    const db = client.db(dbName);
-
-    // new user
-    var name = 'user' + Math.floor(Math.random()*10000);
-    var email = name + '@mit.edu';
-
-    // insert into customer table
-    var collection = db.collection('customers');
-    var doc = {name, email};
-    collection.insertOne(doc, {w:1}, function(err, result) {
-        console.log('Document insert');
-    });
-
-    var customers = db
-        .collection('customers')
-        .find()
-        .toArray(function(err, docs) {
-            console.log('Collection:',docs);
-
-            // clean up
-            client.close();            
-    });    
-
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://<username>:<password>@badbank.svsyh6u.mongodb.net/?retryWrites=true&w=majority";
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
 });
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+// const MongoClient = require('mongodb').MongoClient;
+// const url = 'mongodb://localhost:3000';
+ 
+// // connect to mongo
+// MongoClient.connect(url, {useUnifiedTopology: true}, function(err, client) {
+//   console.log("Connected successfully to server");
+
+//     // database Name
+//     const dbName = 'myproject';
+//     const db = client.db(dbName);
+
+//     // new user
+//     var name = 'user' + Math.floor(Math.random()*10000);
+//     var email = name + '@mit.edu';
+
+//     // insert into customer table
+//     var collection = db.collection('customers');
+//     var doc = {name, email};
+//     collection.insertOne(doc, {w:1}, function(err, result) {
+//         console.log('Document insert');
+//     });
+
+//     var customers = db
+//         .collection('customers')
+//         .find()
+//         .toArray(function(err, docs) {
+//             console.log('Collection:',docs);
+
+//             // clean up
+//             client.close();            
+//     });    
+
+// });
